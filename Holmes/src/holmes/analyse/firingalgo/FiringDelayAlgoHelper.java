f252516bad991473788c18be1514da3b1c042087
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2025-07-05 11:15:07 +0200

    poprawki

--- Holmes/src/holmes/analyse/firingalgo/FiringDelayAlgoHelper.java
+++ Holmes/src/holmes/analyse/firingalgo/FiringDelayAlgoHelper.java
@@ -8,16 +8,31 @@ import java.util.HashMap;
 
 public class FiringDelayAlgoHelper {
 
+    /// oznacza tranzycje jako odwiedzoną na podstawie poprzednich tranzycji
     public static void markTransition(Transition transition) {
         FiringDelayAlgoStateHolder stateHolder = FiringDelayAlgoStateHolder.getInstance();
         if (!stateHolder.isMarked(transition)) {
             return;
         }
 
+        if (isSourceTransition(transition)) {
+            stateHolder.markTransition(transition, null, 0);
+            return;
+        }
+
         HashMap<Integer, Double> markValue = new HashMap<>();
         for (Place place : transition.getInputPlaces()) {
             for (Transition inputTransition : place.getInputTransitions()) {
-                 markValue.putAll(stateHolder.getWeight(inputTransition));
+                double multiplier = (double) inputTransition.getOutputArcWeightTo(place) / transition.getInputArcWeightFrom(place);
+                HashMap<Integer, Double> weights = stateHolder.getWeight(inputTransition);
+                for (Integer conflictPlaceId : weights.keySet()) {
+                    //TODO synchronizacja
+                    markValue.put(conflictPlaceId, weights.get(conflictPlaceId) * multiplier);
+                }
+            }
+
+            if (isConflictPlace(place)) {
+                markValue.put(place.getID(), (double) transition.getInputArcWeightFrom(place));
             }
         }
         stateHolder.markTransition(transition, markValue);
@@ -29,5 +44,13 @@ public class FiringDelayAlgoHelper {
                 return false;
         }
         return true;
+    }
+
+    public static boolean isSourceTransition(Transition transition) {
+        return transition.getInputArcs().size() <= 0;
+    }
+
+    public static boolean isConflictPlace(Place place) {
+        return place.getOutputTransitions().size() > 1;
     }
 }
